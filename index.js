361d9e34ba03f710b0d9670cc3d6b094f3952c28
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-06 22:36:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(katzDeliLine) {
 if (katzDeliLine.length === 0 ){
   return "The line is currently empty."
 } else {
-
+  let i = 0;
   return (`The line is currently: ${i+1}.  `)
 }
 
