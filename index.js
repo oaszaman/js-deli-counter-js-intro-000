87ba060c5a311e74ef1087cc5b63bf36cd1a6e6e
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-06 22:26:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 
 }
 
-function nowServing() {
-
+function nowServing(katzDeliLine) {
+if (katzDeliLine.length === 0) {
+  return "There is nobody waiting to be served!"
+} else {
+  return (`Currently serving ${katzDeliLine.shift()}.`);
+}
 
 }
 
