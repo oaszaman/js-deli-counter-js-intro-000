36f8c77f039c930fd8601c7fd5b68f46fdd66e1b
Shahzab Zaman <o.aszaman00@gmail.com>
2021-04-06 22:16:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function takeANumber(current, newName) {
+var katzDeliLine = [];
+function takeANumber(current, name) {
+katzDeliLine.push(name);
+return 'Welcome,'+ name + '. You are number ' + current.lenth + ' in line.';
 
 
 }
